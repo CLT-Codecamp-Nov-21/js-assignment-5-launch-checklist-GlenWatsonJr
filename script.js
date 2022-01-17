d5e4e421ae276a32e0396ed3d2932180e2fd1eeb
Glen Watson <glenwatsonjr@gmail.com>
2022-01-17 05:03:38 -0500

    test changed id so all will be hidden

--- script.js
+++ script.js
@@ -4,9 +4,8 @@
 
 window.addEventListener("load", function() {
     let form = document.querySelector("form");
-    let list = document.getElementById("list");
-
-   
+    let list = document.getElementById("faultyItems");
+    list.style.visibility ="hidden";  
 
     
   
@@ -18,7 +17,7 @@ window.addEventListener("load", function() {
     
    })
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    list.style.visibility ="hidden";
+   
 
    form.addEventListener("submit", function(event) {
     event.preventDefault();
