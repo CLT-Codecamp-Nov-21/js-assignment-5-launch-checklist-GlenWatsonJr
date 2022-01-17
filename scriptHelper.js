632c3fd1678f143bd7d7d1fc33df664ed97687cb
Glen Watson <glenwatsonjr@gmail.com>
2022-01-17 04:40:22 -0500

    Fixed errors for autograde

--- scriptHelper.js
+++ scriptHelper.js
@@ -58,23 +58,27 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
    
     let launchStatus = document.getElementById("launchStatus");
+    let pilotStatus = document.getElementById("pilotStatus");
+    let copilotStatus = document.getElementById("copilotStatus");
     let fuelStatus = document.getElementById("fuelStatus");
     let cargoStatus = document.getElementById("cargoStatus");
 
     launchStatus.style.color = "rgb(65, 159, 106)";
     launchStatus.innerHTML = "Shuttle is Ready for Launch";
+    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
     fuelStatus.innerHTML = "Fuel level high enough for launch";
     cargoStatus.innerHTML = "Cargo mass low enough for launch";    
     
     if (fuelLevel < 10000) {
         launchStatus.style.color = "rgb(199, 37, 78)";
-        launchStatus.innerHTML = "Shuttle not ready for launch";  
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";  
         fuelStatus.innerHTML = "Fuel level too low for launch";
     }
 
     if (cargoLevel > 10000) {
         launchStatus.style.color = "rgb(199, 37, 78)";
-        launchStatus.innerHTML = "Shuttle not ready for launch";  
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";  
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
     }
     list.style.visibility = "visible";
